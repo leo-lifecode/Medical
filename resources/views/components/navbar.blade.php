bec75ccaffc83c30e5e4344ae9f3a187ce8eb1fc
Fahryzal <fahrjilo@gmail.com>
2024-11-02 22:47:08 +0700

    feat(homepage) : implement script js when scroll section give a active-link

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -7,22 +7,22 @@
             aria-expanded="false">
             <img src={{ asset('assets/img/svg/burger.svg') }} alt="">
         </button>
-        <div class="collapse navbar-collapse" id="mynavbar">
+        <div class="collapse navbar-collapse nav__menu" id="mynavbar">
             <ul class="navbar-nav ms-auto d-flex justify-content-center align-items-center gap-1">
                 <li class="nav-item">
-                    <a class="nav-link text-white flex-shrink-0 d-flex" href="#!">About us</a>
+                    <a class="nav-link text-white flex-shrink-0 d-flex" href="#aboutus">About us</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="#!">Services</a>
+                    <a class="nav-link text-white" href="#services">Services</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="#!">Doctors</a>
+                    <a class="nav-link text-white" href="#doctors">Doctors</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="#!">Contact</a>
+                    <a class="nav-link text-white" href="#contact">Contact</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="#!">Appointment</a>
+                    <a class="nav-link text-white" href="#appointment">Appointment</a>
                 </li>
                 <button type="button" class="btn-nav btn btn-primary px-3 rounded flex-shrink-0">Sign In</button>
                 <button type="button" class="btn-nav btn btn-dark border-0 rounded px-3 flex-shrink-0">Sign Up</button>
--- resources/views/layout/homelayout.blade.php
+++ resources/views/layout/homelayout.blade.php
@@ -32,12 +32,15 @@
     </header>
 
     @yield('content')
-
+    <section id="aboutus"></section>
+    <section id="services"></section>
+    <section id="doctors"></section>
     <x-footer></x-footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
     </script>
+    <script src={{asset('assets/js/script.js')}}></script>
 </body>
 
 </html>
